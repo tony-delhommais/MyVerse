cc05fe19042bb0f7b7e69a63cb846cd0c99d9c0e
Tony Delhommais <tony.delhommais@gmail.com>
2022-08-07 10:53:18 +0200

    small changes in terrain generation script

--- scripts/terrain_generator.py
+++ scripts/terrain_generator.py
@@ -3,11 +3,11 @@ import json
 x_chunck_coord = 0
 y_chunck_coord = 0
 
-chunck_width = 50
-chunck_height = 50
+chunck_width = 100
+chunck_height = 100
 
-x_cell_count = 10
-y_cell_count = 10
+x_cell_count = 20
+y_cell_count = 20
 
 
 cell_width = chunck_width / x_cell_count
@@ -16,7 +16,7 @@ cell_height = chunck_height / y_cell_count
 x_min_chunck_coord = (-1 * chunck_width / 2) + (x_chunck_coord * chunck_width)
 y_min_chunck_coord = (-1 * chunck_height / 2) + (y_chunck_coord * chunck_height)
 
-output_file_name = "land_" + str(x_chunck_coord) + "_" + str(y_chunck_coord) + ".terrain"
+output_file_name = "chunck_" + str(x_chunck_coord) + "_" + str(y_chunck_coord) + ".terrain"
 
 
 def generate_rectangular():
@@ -25,24 +25,18 @@ def generate_rectangular():
 
     for i in range(x_cell_count):
         for y in range(y_cell_count):
-            tri_1 = {}
-            tri_2 = {}
-
             a = [x_min_chunck_coord + (i * cell_width), y_min_chunck_coord + (y * cell_height), 0.0]
             b = [a[0], a[1] + cell_height, a[2]]
             c = [a[0] + cell_width, a[1] + cell_height, a[2]]
             d = [a[0] + cell_width, a[1], a[2]]
-
-            tri_1["coord_1"] = a
-            tri_1["coord_2"] = b
-            tri_1["coord_3"] = c
-
-            tri_2["coord_1"] = a
-            tri_2["coord_2"] = c
-            tri_2["coord_3"] = d
-
-            land.append(tri_1)
-            land.append(tri_2)
+            
+            land.append(a)
+            land.append(b)
+            land.append(c)
+            
+            land.append(a)
+            land.append(c)
+            land.append(d)
 
     data["Land"] = land
 
@@ -56,38 +50,32 @@ def generate_isoceles():
 
     for i in range(x_cell_count):
         for y in range(y_cell_count):
-            tri_1 = {}
-            tri_2 = {}
-
             if y % 2 == 0:
                 a = [x_min_chunck_coord + (i * cell_width), y_min_chunck_coord + (y * cell_height), 0.0]
                 b = [a[0] + (cell_width / 2), a[1] + cell_height, a[2]]
                 c = [a[0] + cell_width + (cell_width / 2), a[1] + cell_height, a[2]]
                 d = [a[0] + cell_width, a[1], a[2]]
 
-                tri_1["coord_1"] = a
-                tri_1["coord_2"] = b
-                tri_1["coord_3"] = d
-
-                tri_2["coord_1"] = b
-                tri_2["coord_2"] = c
-                tri_2["coord_3"] = d
+                land.append(a)
+                land.append(b)
+                land.append(d)
+                
+                land.append(b)
+                land.append(c)
+                land.append(d)
             else:
                 a = [x_min_chunck_coord + (i * cell_width) + (cell_width / 2), y_min_chunck_coord + (y * cell_height), 0.0]
                 b = [a[0] - (cell_width / 2), a[1] + cell_height, a[2]]
                 c = [a[0] + cell_width - (cell_width / 2), a[1] + cell_height, a[2]]
                 d = [a[0] + cell_width, a[1], a[2]]
 
-                tri_1["coord_1"] = a
-                tri_1["coord_2"] = b
-                tri_1["coord_3"] = c
-
-                tri_2["coord_1"] = a
-                tri_2["coord_2"] = c
-                tri_2["coord_3"] = d
-
-            land.append(tri_1)
-            land.append(tri_2)
+                land.append(a)
+                land.append(b)
+                land.append(c)
+                
+                land.append(a)
+                land.append(c)
+                land.append(d)
 
     data["Land"] = land
 
